e144ee9702ca46d49707676948e80ac889e0e333
CForrest97 <craforrest97@gmail.com>
2022-12-03 16:43:31 +0000

    make day 01 a one liner b'cus

--- src/days/01/index.ts
+++ src/days/01/index.ts
@@ -1,27 +1,15 @@
-import { MinPriorityQueue } from "@datastructures-js/priority-queue";
-import { compose, map, split, sum } from "ramda";
+import { compose, map, negate, sortBy, split, sum, take } from "ramda";
 import { parseNumbers } from "../../helpers/parsers";
 
-const parseInventories = compose(map(parseNumbers), split("\n\n"));
+const sumMaxCalories = (numberOfElves: number) =>
+  compose(
+    sum,
+    take(numberOfElves),
+    sortBy(negate),
+    map(sum),
+    map(parseNumbers),
+    split("\n\n")
+  );
 
-const sumMaxCalories = (
-  inventories: number[][],
-  numberOfElves: number
-): number => {
-  const caloriesQueue = new MinPriorityQueue<number>();
-
-  const pushToQueue = (calories: number) => {
-    caloriesQueue.push(calories);
-    if (caloriesQueue.size() > numberOfElves) caloriesQueue.pop();
-  };
-
-  const calorieTotals = map(sum, inventories);
-  calorieTotals.forEach(pushToQueue);
-
-  return sum(caloriesQueue.toArray());
-};
-
-export const solvePart1 = (input: string) =>
-  sumMaxCalories(parseInventories(input), 1);
-export const solvePart2 = (input: string) =>
-  sumMaxCalories(parseInventories(input), 3);
+export const solvePart1 = sumMaxCalories(1);
+export const solvePart2 = sumMaxCalories(3);
