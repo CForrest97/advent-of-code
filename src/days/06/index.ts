59400b9095fd1596cc031590076bce6a53a00aa9
CForrest97 <craforrest97@gmail.com>
2022-12-07 22:41:20 +0000

    rename solve fn

--- src/days/06/index.ts
+++ src/days/06/index.ts
@@ -1,11 +1,11 @@
 import { range } from "ramda";
 
-const solve = (n: number) => (input: string) => {
+const getSizeOfMarker = (n: number) => (input: string) => {
   for (let i = 0; ; i += 1) {
     const set = new Set(range(i, i + n).map((j) => input.charAt(j)));
     if (set.size === n) return i + n;
   }
 };
 
-export const solvePart1 = solve(4);
-export const solvePart2 = solve(14);
+export const solvePart1 = getSizeOfMarker(4);
+export const solvePart2 = getSizeOfMarker(14);
